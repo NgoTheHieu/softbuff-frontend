daab08d6ee46775f4f5d235a35618805431070f9
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2020-07-21 16:49:51 +0700

    124421

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -4,15 +4,20 @@ import { MDBMedia } from 'mdbreact';
 const MediaObjectPage = (props) => {
   return (
     <MDBMedia>
+      
       <MDBMedia left className="mr-3" href="#">
         <MDBMedia object src="https://mdbootstrap.com/img/Photos/Others/placeholder1.jpg" alt="" />
       </MDBMedia>
+      
       <MDBMedia body>
         <MDBMedia heading>
+          
           From {props.user} comments that:
       </MDBMedia>
        <h1>{props.body}</h1>
        <h5>At :{props.createdAt}</h5>
+       
+      <hr className="my-4" />
     </MDBMedia>
     </MDBMedia>
   );
--- src/pages/AlgoDetail.js
+++ src/pages/AlgoDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import CreateQuizForm from "../components/CreateQuizForm";
@@ -112,7 +112,7 @@ export default function AlgoDetail(props) {
   };
   return (
     <div>
-      <Navbar noview={true} />
+      <Navbar noview={true} user={user} />
       <div className="bodyimage">
         <MDBContainer>
           <MDBRow>
